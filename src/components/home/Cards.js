17aee8d7e3196bb28007ce73fb48f343109d5e60
Darshan Aswathappa <aswathappa.d@northeastern.edu>
2024-10-10 13:55:28 -0400

    Revert "Merging main and deployment"

---
+++ src/components/home/Cards.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import 'style/home/Cards.css';
+import accurate from 'assets/images/icons8-accurate-100.png';
+import muscle from 'assets/images/icons8-muscle-100.png';
+import developing from 'assets/images/icons8-developing-100.png';
+import smart from 'assets/images/icons8-smart-100.png';
+
+const Cards = () => {
+	return (
+		<div>
+			<div className="additional-content">
+				<p>Our Gold Digging Models</p>
+				<p>Boost your career systematically</p>
+			</div>
+
+			<div className="container mt-4 mb-5">
+				<div className="row">
+					<div className="col-md-3 col-12 mb-3">
+						<div className="card" style={{ width: '100%' }}>
+							<img src={accurate} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Accurate Role Selection</h5>
+								<p className="card-text">
+									Some quick example text to build on the card title and make up the bulk of the card's content.
+								</p>
+								<a href="#" className="btn btn-filled-orange">
+									Find out more
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3 col-12 mb-3">
+						<div className="card" style={{ width: '100%' }}>
+							<img src={muscle} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Consolidated Résumé Craft</h5>
+								<p className="card-text">
+									Some quick example text to build on the card title and make up the bulk of the card's content.
+								</p>
+								<a href="#" className="btn btn-filled-orange">
+									Find out more
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3 col-12 mb-3">
+						<div className="card" style={{ width: '100%' }}>
+							<img src={developing} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Effective Job Application</h5>
+								<p className="card-text">
+									Some quick example text to build on the card title and make up the bulk of the card's content.
+								</p>
+								<a href="#" className="btn btn-filled-orange">
+									Find out more
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3 col-12 mb-3">
+						<div className="card" style={{ width: '100%' }}>
+							<img src={smart} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">Smart Follow-Up Offer</h5>
+								<p className="card-text">
+									Some quick example text to build on the card title and make up the bulk of the card's content.
+								</p>
+								<a href="#" className="btn btn-filled-orange">
+									Find out more
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	);
+};
+
+export default Cards;
--- src/layout/footer.js
+++ src/layout/footer.js
@@ -2,8 +2,37 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <footer className="bg-primary text-white">footer here</footer>
-    </div>
-  );
+		<div className="container">
+			<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+				<p className="col-md-4 mb-0 text-body-secondary">
+					&copy; 2024 Resume Reviewer
+				</p>
+
+				<a
+					href="/"
+					className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+				>
+					<img src="./assets/logos/windows.png" className="bi me-w" width={40} height={40} />
+				</a>
+
+				<ul className="nav col-md-4 justify-content-end">
+					<li className="nav-item">
+						<a href="/" className="nav-link px-2 text-body-secondary">
+							Home
+						</a>
+					</li>
+					<li className="nav-item">
+						<a href="/" className="nav-link px-2 text-body-secondary">
+							FAQs
+						</a>
+					</li>
+					<li className="nav-item">
+						<a href="/" className="nav-link px-2 text-body-secondary">
+							About
+						</a>
+					</li>
+				</ul>
+			</footer>
+		</div>
+	);
 }
