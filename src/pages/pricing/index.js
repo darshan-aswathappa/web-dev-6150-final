3eeebc8ebbd22c3f6e3f4461417108cc032744d9
xanf-code <aswathappa.d@northeastern.edu>
2024-10-10 15:08:16 -0400

    removed cards for layout fix

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -26,7 +26,7 @@ export default function Pricing() {
         <main>
           <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
             <div className="col">
-              <div className="card mb-4 rounded-3 shadow-sm">
+              <div className=" mb-4 rounded-3 shadow-sm">
                 <div className="card-header py-3">
                   <h4 className="my-0 fw-normal">Free</h4>
                 </div>
@@ -43,7 +43,7 @@ export default function Pricing() {
               </div>
             </div>
             <div className="col">
-              <div className="card mb-4 rounded-3 shadow-sm">
+              <div className=" mb-4 rounded-3 shadow-sm">
                 <div className="card-header py-3">
                   <h4 className="my-0 fw-normal">Pro</h4>
                 </div>
@@ -60,7 +60,7 @@ export default function Pricing() {
               </div>
             </div>
             <div className="col">
-              <div className="card mb-4 rounded-3 shadow-sm border-primary">
+              <div className=" mb-4 rounded-3 shadow-sm border-primary">
                 <div className="card-header py-3 text-bg-primary border-primary">
                   <h4 className="my-0 fw-normal">Enterprise</h4>
                 </div>
