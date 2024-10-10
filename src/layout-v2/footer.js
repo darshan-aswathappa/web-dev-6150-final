4efe7f43aaf0b077fa1e4f04f68eccc3308e51c2
louyanqing <lou.yan@northeastern.edu>
2024-10-10 12:48:50 -0400

    created the home-v2 and layout-v2

---
+++ src/layout-v2/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className=" row footer-wrapper">
+            <div className="footer-text">
+                <strong>©Vérifier de Résumé 2024 | All Rights Reserved | Contact Us: </strong>
+                <a href="tel:+13009445628">vérifierderésumé@northeastern.edu</a>
+            </div>
+            <div className="footer-legal">
+                <a href="#" className="footer-link">Privacy Policy</a>
+                <a href="#" className="footer-link">Terms of Use</a>
+            </div>
+        </footer>
+    );
+}
---
+++ src/layout-v2/main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./footer";
+import Header from "./header";
+import VantaBackground from "../pages/home-v2/vanta-background";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="d-flex flex-column min-vh-100 App">
+      <VantaBackground />
+      <header>
+        <Header />
+      </header>
+      <main>{children}</main>
+      <footer className="mt-auto">
+        <Footer />
+      </footer>
+    </div>
+  );
+}
